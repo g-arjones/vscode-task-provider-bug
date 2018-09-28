a6badb011586a8fe2694f266b9c62871025396ef
Gabriel Arjones <arjones@arjones.com>
2018-09-28 13:52:30 -0300

    add task to test Terminal.processId property

--- src/extension.ts
+++ src/extension.ts
@@ -14,13 +14,18 @@ export function activate(_context: vscode.ExtensionContext): void {
             const scope = vscode.workspace.workspaceFolders[0];
             const quickTaskName = "Quick Task"
             const slowTaskName = "Slow Task"
+            const terminalTaskName = "Terminal Test Task"
             const quickExecution = new vscode.ProcessExecution("/bin/false");
             const slowExecution = new vscode.ProcessExecution("/bin/sleep", ["10000"]);
+            const terminalExecution = new vscode.ShellExecution("sleep 2 && echo $$");
+
             const quickKind: TestTaskDefinition = { type: 'test', task: quickTaskName };
             const slowKind: TestTaskDefinition = { type: 'test', task: slowTaskName };
+            const terminalKind: TestTaskDefinition = { type: 'test', task: terminalTaskName };
 
             return [new vscode.Task(quickKind, scope, quickTaskName, 'test', quickExecution, []),
-                    new vscode.Task(slowKind, scope, slowTaskName, 'test', slowExecution, [])];
+                    new vscode.Task(slowKind, scope, slowTaskName, 'test', slowExecution, []),
+                    new vscode.Task(terminalKind, scope, terminalTaskName, 'test', terminalExecution, [])];
         },
         resolveTask(_task: vscode.Task): vscode.Task | undefined {
             return undefined;
@@ -30,6 +35,13 @@ export function activate(_context: vscode.ExtensionContext): void {
     vscode.tasks.onDidEndTaskProcess((event: vscode.TaskProcessEndEvent) => {
         console.log(`Exit code: ${event.exitCode}`);
     });
+
+    vscode.window.onDidOpenTerminal((terminal) => {
+        terminal.onDidWriteData(async (data) => {
+            const processId = await terminal.processId;
+            console.log(`${processId} === ${data}`);
+        })
+    })
 }
 
 export function deactivate(): void {
